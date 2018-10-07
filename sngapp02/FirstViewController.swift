13ec01ff37314a552febd036eb7b7cf56f30a9ba
祝钧扬 <jlp@zhujunyangdeimac.local>
2018-10-08 10:52:57 +1100

    Initial Commit

---
+++ sngapp02/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  sngapp02
+//
+//  Created by 祝钧扬 on 8/10/18.
+//  Copyright © 2018 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sngapp02/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  sngapp02
+//
+//  Created by 祝钧扬 on 8/10/18.
+//  Copyright © 2018 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
